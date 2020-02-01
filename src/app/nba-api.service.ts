3fba2abed16d4b3376979ad25183c069cd93d29e
Louis Sutton <louis.sutton.iii@gmail.com>
2020-01-31 20:55:57 -0800

    Added Stat Variables

--- src/app/nba-api.service.ts
+++ src/app/nba-api.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
-
 @Injectable({
   providedIn: 'root'
 })
 export class NbaApiService {
-
+playerStats;
+TeamStats;
+LeagueStats;
   constructor() { }
 }
